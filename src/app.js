11430d7aa5ef751c167d88874bc6fefc659dbb52
Eduardo <du_du.mba@hotmail.com>
2020-07-24 19:54:33 +0100

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,16 +11,56 @@ app.use(cors());
 
 const repositories = [];
 
+//id verification
+function validateRepositoriesId(request, response, next){
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: "Invalid ID."});
+  }
+
+  return next();
+}
+
+app.use("/repositories/:id", validateRepositoriesId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  let likes = 0;
+
+  const repositorie = { id: uuid(), title, url, techs, likes };
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  const repositorieId = repositories.findIndex(repositorie => repositorie.id === id);
+  
+  if(repositorieId < 0) {
+    return response.status(400).json({ error: "Project not found."}); 
+  }
+  
+  const repositorie = {
+    title,
+    url,
+    techs,
+  }
+
+  repositories[repositorieId] = repositorie;
+
+  return response.json(repositorie);
+
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -29,5 +70,9 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
 });
+
+// app.listen(3334, () => {
+//   console.log("Back-end started!");
+// })
 
 module.exports = app;
